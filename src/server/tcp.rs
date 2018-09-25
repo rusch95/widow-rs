d0328ed0406bf2923299d8285e3fee7acc1a2994
Robert Rusch <rusch@mit.edu>
2018-09-25 14:28:30 -0400

    Inc

--- src/server/tcp.rs
+++ src/server/tcp.rs
@@ -1,36 +1,35 @@
 use std::io;
 use std::io::{Read, Write};
-use std::net::{Ipv4Addr, SocketAddrV4, TcpListener, TcpStream};
+use std::net::{Ipv4Addr, SocketAddrV4};
 use std::sync::mpsc::channel;
 use std::sync::mpsc::Receiver;
 use std::sync::mpsc::Sender;
 use std::thread;
 
 use bincode::{deserialize, serialize};
+use tokio;
 use tokio::net::{TcpStream, TcpListener};
 use tokio::prelude::*;
 
 use consts::*;
 use utils::*;
 
-pub type StreamOutCh = Sender<FnCall>;
-pub type StreamInCh = Receiver<FnRes>;
-pub type ServerOutCh = Sender<FnRes>;
-pub type ServerInCh = Receiver<FnCall>;
-pub type NewStreamCh = (ServerOutCh, ServerInCh);
-pub type NewStreamOutCh = Sender<NewStreamCh>;
-pub type NewStreamInCh = Receiver<NewStreamCh>;
+pub type ListenerOutCh = Sender<(u32, FnCall)>;
+pub type HandlerInCh = Receiver<(u32, FnCall)>;
+pub type HandlerOutCh = Sender<(u32, FnRes)>;
+pub type ListenerInCh = Receiver<(u32, Fnres)>;
 
 // Spawns threads and such
 pub fn init_widow_server(server_ip: Ipv4Addr, port: u16) {
-    let (new_stream_outbox, new_stream_inbox) = channel();
+    let (listener_outbox, handler_inbox) = channel();
+    let (handler_outbox, listener_inbox) = channel();
 
-    thread::spawn(move || init_widow_listener(server_ip, port, new_stream_outbox);
-    thread::spawn(move || server.start(new_stream_inbox));
+    thread::spawn(move || init_widow_listener(server_ip, port, listener_outbox, listener_inbox));
+    thread::spawn(move || init_widow_handler(handler_outbox, handler_inbox));
 }
 
 /* Start up a tokio::TcpListener. Does not return */
-pub fn init_widow_listener(server_ip: Ipv4Addr, port: u16, outbox: NewStreamOutCh) {
+pub fn init_widow_listener(server_ip: Ipv4Addr, port: u16, outbox: ListenerOutCh, inbox: ListenerInCh) {
     let tcp_conn = SocketAddrV4::new(server_ip, port);
     let listener = TcpListener::bind(tcp_conn).unwrap();
     info!("Listening on {:?}", tcp_conn);
@@ -39,35 +38,26 @@ pub fn init_widow_listener(server_ip: Ipv4Addr, port: u16, outbox: NewStreamOutC
         listener.incoming()
             .map_err(|e| error!("Failed to accept socket; error = {:?}", e))
             .for_each(|socket| {
-                WidowStream(socket).start();
+                WidowStream::new(socket).start();
                 Ok(())
-            }
+        });
     });
 }
 
-init_widow_server(new_stream_inbox: NewStreamInCh) -> WidowServer {
-        let streams = Vec::new(),
-        let state = State::new(),
+pub fn init_widow_handler(outbox: HandlerOutCh, inbox: HandlerInCh) {
+    let state = State::new();
 
-        loop {
-            // Look for new streams
-            if let Ok(new_stream) = self.new_stream_inbox.try_recv() {
-                self.streams.push(new_stream);
-            }
-
-            // Service messages from streams
-            for (outbox, inbox) in &mut self.streams {
-                if let Ok(fncall) = inbox.try_recv() {
-                    let res = self.state.dispatch(fncall);
-                    outbox.send(res).unwrap();
-                }
-            }
+    // Service messages from streams
+    for (stream_id, fncall) in &mut inbox.recv() {
+        if let Ok(fncall) = inbox.try_recv() {
+            let res = .state.dispatch(fncall);
+            outbox.send(res).unwrap();
         }
     }
 }
 
 pub struct WidowStream {
-    stream: TcpStream,
+    socket: TcpStream,
     inbox: StreamInCh,
     outbox: StreamOutCh,
 }
@@ -75,7 +65,7 @@ pub struct WidowStream {
 impl WidowStream {
     pub fn new(socket: TcpStream, inbox: StreamInCh, outbox: StreamOutCh) -> WidowStream {
         WidowStream {
-            stream,
+            socket,
             inbox,
             outbox,
         }
@@ -104,16 +94,16 @@ impl WidowStream {
 
         buf[..4].clone_from_slice(&enc_size_u8s);
         buf[4..buf_len].clone_from_slice(&encoded);
-        let _amt = self.stream.write(&buf[..buf_len]);
+        let _amt = self.socket.write(&buf[..buf_len]);
     }
 
     fn rcv(&mut self) -> Result<FnCall, io::Error> {
         let mut n_buf = [0u8; 4];
         let mut buf = [0u8; MSG_BUF_SIZE];
 
-        try!(self.stream.read_exact(&mut n_buf));
+        try!(self.socket.read_exact(&mut n_buf));
         let n = u8_array_to_usize(&n_buf[..], 0);
-        try!(self.stream.read_exact(&mut buf[..n]));
+        try!(self.socket.read_exact(&mut buf[..n]));
 
         let fncall: FnCall = deserialize(&buf[..n]).unwrap();
 
